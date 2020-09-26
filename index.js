9d007050023424b632b04f807163d723412892e5
Theresa Rutledge <t.roserutledge@gmail.com>
2020-09-25 19:18:25 -0700

    MVP

--- index.js
+++ index.js
@@ -1,40 +1,136 @@
-let addGroup = document.querySelector('#groups');
 
-function addGroupHandler (event){
+
+// const {availableRecipients,assignRecipients} = require('./script.js');
+let memberArray = [];
+
+const addGroup = document.querySelector('#groups');
+const generate = document.querySelector('#group-form');
+const results = document.querySelector('#results');
+
+function addGroupHandler(event) {
     event.preventDefault();
-    if(event.target.tagName==='BUTTON'){
+    if (event.target.tagName === 'BUTTON') {
         let id = event.target.id.split('');
-        groupNumber= (id[id.length-1].trim());
+        groupNumber = (id[id.length - 1].trim());
         groupNumber = parseInt(groupNumber);
         let removeButton = document.querySelector(`#button-${groupNumber}`);
         removeButton.remove();
         groupNumber++;
-//remove buton that was clicked
 
+        let newDiv = document.createElement('div');
+        newDiv.classList.add('form-inline');
 
-let newDiv = document.createElement('div');
-newDiv.classList.add('form-inline');
+        let newLabel = document.createElement('label');
+        newLabel.setAttribute('for', `group-${groupNumber}`);
+        newLabel.innerText = `Group ${groupNumber}: `;
 
-let newLabel = document.createElement('label');
-newLabel.setAttribute('for',`group-${groupNumber}`);
-newLabel.innerText = `Group ${groupNumber}: `;
+        let newInput = document.createElement('input');
+        newInput.setAttribute('type', 'text')
+        newInput.setAttribute('id', `group-${groupNumber}`);
+        newInput.classList.add('form-control', 'mb-2', 'mr-sm-2');
 
-let newInput = document.createElement('input');
-newInput.setAttribute('type','text')
-newInput.setAttribute('id',`group-${groupNumber}`);
-newInput.classList.add('form-control', 'mb-2', 'mr-sm-2');
+        let newButton = document.createElement('button');
+        newButton.setAttribute('id', `button-${groupNumber}`);
+        newButton.classList.add('btn', 'btn-primary', 'mb-2');
+        newButton.innerText = "Add Group";
 
-let newButton = document.createElement('button');
-newButton.setAttribute('id',`button-${groupNumber}`);
-newButton.classList.add('btn', 'btn-primary', 'mb-2');
-newButton.innerText = "Add Group";
+        newDiv.appendChild(newLabel);
+        newDiv.appendChild(newInput);
+        newDiv.appendChild(newButton);
 
-newDiv.appendChild(newLabel);
-newDiv.appendChild(newInput);
-newDiv.appendChild(newButton);
-
-addGroup.appendChild(newDiv);
+        addGroup.appendChild(newDiv);
     }
 }
 
-addGroup.addEventListener('click',addGroupHandler);
+function formSubmitHandler(event) {
+    event.preventDefault();
+    const groupItems = addGroup.children;
+    const groupArray = Array.from(groupItems);
+    if (groupArray.length < 2) {
+        alert('Must enter at least two groups');
+        return;
+    }
+
+    //go through each group and add the members to the member array
+    groupArray.forEach((group) => {
+        let id = group.children[1].id;
+        groupNumber = (id[id.length - 1].trim());
+        groupNumber = parseInt(groupNumber);
+        let names = group.children[1].value;
+            names = names.split(',');
+            for (i = 0; i < names.length; i++) {
+                memberArray.push({
+                    name: names[i],
+                    group: groupNumber,
+                    assigned: false
+                })
+            }
+
+    })
+assignRecipients();
+}
+
+
+
+
+
+
+
+
+// function to get available recipients. Can't be in same group or already be assigned
+const availableRecipients = (giver) => {
+    let recipientsArray = [];
+    for (i = 0; i < memberArray.length; i++) {
+        if (memberArray[i].assigned === false && memberArray[i].group !== giver.group) {
+
+            recipientsArray.push(memberArray[i]);
+            recipientsArray[recipientsArray.length - 1].index = i;
+        }
+    }
+    return recipientsArray;
+}
+
+//assignRecipient: generate random recipient from available array. Change that recipient's assigned to true
+// and update giver's recipient value.
+const assignRecipients = () => {
+    for (j = 0; j < memberArray.length; j++) {
+
+        let giver = memberArray[j];
+
+        let availableRecipientsArray = availableRecipients(giver);
+
+        let randomNumber = Math.floor(Math.random() * availableRecipientsArray.length);
+
+        let randomRecipient = availableRecipientsArray[randomNumber];
+        memberArray[randomRecipient.index].assigned = true;
+        giver.recipient = randomRecipient.name;
+
+
+    }
+    displayResults();
+}
+
+function displayResults() {
+    for (i = 0; i < memberArray.length; i++) {
+        let newResult = document.createElement('h4');
+        newResult.innerText = `${memberArray[i].name} gives to ${memberArray[i].recipient}!`;
+        results.appendChild(newResult);
+    }
+    let startOver = document.createElement('button');
+    startOver.classList.add('btn', 'btn-primary');
+    startOver.setAttribute('id', 'start-over');
+    startOver.innerText = 'Start Over';
+
+    results.appendChild(startOver);
+
+    generate.remove();
+
+}
+
+function startOverHandler(event) {
+    location.reload();
+}
+
+addGroup.addEventListener('click', addGroupHandler);
+generate.addEventListener('submit', formSubmitHandler);
+results.addEventListener('click', startOverHandler);
--- script.js
+++ script.js
@@ -1,45 +1,45 @@
 // eventually the members array will be a table
 
 
-const memberArray = [
-    {
-        name: 'Eric',
-        group: 1,
-        assigned: false,
+// const memberArray = [
+//     {
+//         name: 'Eric',
+//         group: 1,
+//         assigned: false,
 
-    },
-    {
-        name: 'Theresa',
-        group: 1,
-        assigned: false
-    },
-    {
-        name: 'Brad',
-        group: 2,
-        assigned: false
-    },
-    {
-        name: 'Jane',
-        group: 2,
-        assigned: false
-    },
-    {
-        name: 'Jeff',
-        group: 3,
-        assigned: false
-    },
-    {
-        name: 'Sarah',
-        group: 3,
-        assigned: false
-    },
-    {
-        name: 'Leighton',
-        group: 3,
-        assigned: false
-    },
+//     },
+//     {
+//         name: 'Theresa',
+//         group: 1,
+//         assigned: false
+//     },
+//     {
+//         name: 'Brad',
+//         group: 2,
+//         assigned: false
+//     },
+//     {
+//         name: 'Jane',
+//         group: 2,
+//         assigned: false
+//     },
+//     {
+//         name: 'Jeff',
+//         group: 3,
+//         assigned: false
+//     },
+//     {
+//         name: 'Sarah',
+//         group: 3,
+//         assigned: false
+//     },
+//     {
+//         name: 'Leighton',
+//         group: 3,
+//         assigned: false
+//     },
 
-]
+// ]
 
 // function to get available recipients. Can't be in same group or already be assigned
 const availableRecipients = (giver) => {
@@ -75,13 +75,12 @@ const assignRecipients = () => {
 
 
 //print member.recipient list
-const printResults = () => {
-    assignRecipients();
-    for (i = 0; i < memberArray.length; i++) {
-        console.log(`${memberArray[i].name}: ${memberArray[i].recipient}`);
-    }
-}
+// const printResults = () => {
+//     assignRecipients();
+//     for (i = 0; i < memberArray.length; i++) {
+//         console.log(`${memberArray[i].name}: ${memberArray[i].recipient}`);
+//     }
+// }
 
-// printResults();
 
-assignRecipients();
+module.exports = {availableRecipients,assignRecipients};
