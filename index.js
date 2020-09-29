452ee61064c76ff21b033eee978e8b0758f30a80
Theresa Rutledge <t.roserutledge@gmail.com>
2020-09-28 17:24:48 -0700

    add validation for group sizing

--- index.js
+++ index.js
@@ -17,33 +17,33 @@ function addGroupHandler(event) {
         groupNumber = (id[id.length - 1].trim());
         groupNumber = parseInt(groupNumber);
 
-        if(document.querySelector(`#group-${groupNumber}`).value){
-        let removeButton = document.querySelector(`#button-${groupNumber}`);
-        removeButton.remove();
-        groupNumber++;
+        if (document.querySelector(`#group-${groupNumber}`).value) {
+            let removeButton = document.querySelector(`#button-${groupNumber}`);
+            removeButton.remove();
+            groupNumber++;
 
-        let newDiv = document.createElement('div');
-        newDiv.classList.add('form-inline');
+            let newDiv = document.createElement('div');
+            newDiv.classList.add('form-inline');
 
-        let newLabel = document.createElement('label');
-        newLabel.setAttribute('for', `group-${groupNumber}`);
-        newLabel.innerText = `Group ${groupNumber}: `;
+            let newLabel = document.createElement('label');
+            newLabel.setAttribute('for', `group-${groupNumber}`);
+            newLabel.innerText = `Group ${groupNumber}: `;
 
-        let newInput = document.createElement('input');
-        newInput.setAttribute('type', 'text')
-        newInput.setAttribute('id', `group-${groupNumber}`);
-        newInput.classList.add('form-control', 'mb-2', 'mr-sm-2');
+            let newInput = document.createElement('input');
+            newInput.setAttribute('type', 'text')
+            newInput.setAttribute('id', `group-${groupNumber}`);
+            newInput.classList.add('form-control', 'mb-2', 'mr-sm-2');
 
-        let newButton = document.createElement('button');
-        newButton.setAttribute('id', `button-${groupNumber}`);
-        newButton.classList.add('btn', 'btn-primary', 'mb-2');
-        newButton.innerText = "Add Group";
+            let newButton = document.createElement('button');
+            newButton.setAttribute('id', `button-${groupNumber}`);
+            newButton.classList.add('btn', 'btn-secondary', 'mb-2');
+            newButton.innerText = "Add Group";
 
-        newDiv.appendChild(newLabel);
-        newDiv.appendChild(newInput);
-        newDiv.appendChild(newButton);
+            newDiv.appendChild(newLabel);
+            newDiv.appendChild(newInput);
+            newDiv.appendChild(newButton);
 
-        addGroupBtn.appendChild(newDiv);
+            addGroupBtn.appendChild(newDiv);
         } else {
             alert('Must add at least one name');
             return;
@@ -51,46 +51,72 @@ function addGroupHandler(event) {
     }
 }
 
+//if any group has more members than all other groups combined send error
+function validateGroups(numOfGroups) {
+    const totalPeople = memberArray.length;
+    let validate =true;
+    for (i = 1; i <= numOfGroups; i++) {
+        let groupTotal = 0;
+        for (j = 0; j < totalPeople; j++) {
+            if (memberArray[j].group === i) {
+                groupTotal++;
+            }
+            if (groupTotal > totalPeople - groupTotal) {
+                alert('Oops! One group has too many recipients.');
+                validate = false
+                return;
+            }
+        }
+    }
+    return validate;
+}
+
+
 function formSubmitHandler(event) {
     event.preventDefault();
     const groupItems = addGroupBtn.children;
+    //array of each group
     const groupArray = Array.from(groupItems);
     if (groupArray.length < 2) {
         alert('Must enter at least two groups');
         return;
     }
-    //if the last group box had a value then go through each group and add the members to the member array
-    if(!groupArray[groupArray.length-1].children[1].value){
-        alert('Must enter at least one name per group');
-        return;
-    }
-
-
+  
+    //create member array
     groupArray.forEach((group) => {
         let id = group.children[1].id;
         groupNumber = (id[id.length - 1].trim());
         groupNumber = parseInt(groupNumber);
         let names = group.children[1].value;
-            names = names.split(',');
-            for (i = 0; i < names.length; i++) {
-                memberArray.push({
-                    name: names[i].trim(),
-                    group: groupNumber,
-                    assigned: false
-                })
-            }
+        if(names){
+        names = names.split(',');
+        for (i = 0; i < names.length; i++) {
+            memberArray.push({
+                name: names[i].trim(),
+                group: groupNumber,
+                assigned: false
+            })
+        }
+    }
 
     })
-assignRecipients();
-displayResults();
+    // validate group distribution will work
+    if (!validateGroups(groupArray.length)) {
+        memberArray =[];
+        return;
+    }
+
+    assignRecipients();
+    displayResults();
 }
 
 const reset = () => {
-for (i=0; i<memberArray.length; i++){
-    memberArray[i].assigned = false;
-}
+    for (i = 0; i < memberArray.length; i++) {
+        memberArray[i].assigned = false;
+    }
     assignRecipients();
 }
+
 
 
 // function to get available recipients. Can't be in same group or already be assigned
@@ -114,15 +140,15 @@ const assignRecipients = () => {
         let giver = memberArray[j];
 
         let availableRecipientsArray = availableRecipients(giver);
-        if(!availableRecipientsArray){
-            reset();
-            return;
 
-        }
 
         let randomNumber = Math.floor(Math.random() * availableRecipientsArray.length);
 
         let randomRecipient = availableRecipientsArray[randomNumber];
+        if (!randomRecipient) {
+            reset()
+            return;
+        }
         memberArray[randomRecipient.index].assigned = true;
         giver.recipient = randomRecipient.name;
 
@@ -137,7 +163,7 @@ function displayResults() {
         resultsDiv.appendChild(newResult);
     }
     let startOver = document.createElement('button');
-    startOver.classList.add('btn', 'btn-primary');
+    startOver.classList.add('btn', 'btn-primary', "mt-3", "mb-2");
     startOver.setAttribute('id', 'start-over');
     startOver.innerText = 'Start Over';
 
@@ -148,8 +174,8 @@ function displayResults() {
 }
 
 function startOverHandler(event) {
-    if(event.target.tagName === 'BUTTON'){
-    location.reload();
+    if (event.target.tagName === 'BUTTON') {
+        location.reload();
     }
 }
 
